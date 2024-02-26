07607d08328f72ca056a9f87f31de41a27b4b4be
RinTeusy <dianka.morozkova@mail.ru>
2024-02-26 17:42:47 +0300

    База данных

--- main/cafe/models.py
+++ main/cafe/models.py
@@ -13,7 +13,7 @@ class Menu(models.Model):
     name = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True)
     image = models.ImageField(null=True, blank=True)
-    # category
+    category = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return str(self.id)
