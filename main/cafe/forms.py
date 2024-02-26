591e3ed5819dd7e8d16363605705dfa0fbfe38c3
Timur Usmanov aka MakaMonstir <fonuatimur@gmail.com>
2024-02-26 11:28:31 +0700

    rework forms and views, change req's versions

--- main/cafe/forms.py
+++ main/cafe/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegisterForm(UserCreationForm):
@@ -17,3 +18,9 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'phone']
+    
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        if User.objects.filter(phone=phone):
+            raise ValidationError("Этот адрес электронной почты уже существует.")
+        return phone
--- main/main/settings.py
+++ main/main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cafe.apps.CafeConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
